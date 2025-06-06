b1ecf45b38b2132b0be941eb401f10c6ce020ddf
Adrian <134459802+adrianandbaton@users.noreply.github.com>
2025-06-06 01:31:39 +0000

    check if the centre card is an action card

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import  random
-cards = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
-         'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
-         'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
-         'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR',]
+import random
+import os 
+deck = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
+'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
+'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
+'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR',
+'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
+'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
+'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
+'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR']
+
+random.shuffle(deck)
+
 rules = input('Do you want to review the rules?(Y/N)')
 while rules != "N" or rules != "Y":
     rules = input("Please reenter with (Y/N)")
@@ -40,5 +48,51 @@ Skip everyone – goes back to your turn
 You cannot play after you draw""")
 
 
-def distribute_cards(deck):
+def distribute_cards(number, deck):
     player_deck = []
+    for i in range(number):
+        x = deck.pop[i]
+        player_deck.append(x)
+
+    return player_deck
+
+def next_turn():
+    ready = input("Has the laptop passed to the next user? (Y?N)")
+    while ready.upper() != 'Y':
+        ready = input("OK, now has the laptop been passed (Y/N)")
+    if ready  == 'Y':
+        os.system('clear')
+
+def draw_card(player_deck, deck):
+    x = deck[0]
+    player_deck.append(x)
+    return player_deck
+
+def play_card(player_deck, center_card, deck):
+    card = input("What card do you wish to place?, press 'd' to draw card")
+    if card == 'd':
+        draw_card(player_deck, deck)
+
+    while card not in player_deck and card != 'd':
+        print("This card is not in your deck, please reenter")
+        card = input("What card do you wish to place?")
+
+    if card in player_deck:
+        while card[0] != center_card[0] and card[1] != center_card[1] and card != 'd':
+            card = input("This card is not valid, please reenter")
+        player_deck.remove(card)
+        center_card = card
+        
+    
+        
+
+
+
+
+def main(deck):
+    center_card = deck[0]
+    if '+2' in center_card or center_card[1] == 'R' or 
+    player1_deck = distribute_cards(7,deck)
+    player2_deck = distribute_cards(7,deck)
+
+    
