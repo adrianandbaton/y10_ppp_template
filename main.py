40f30e1ffb22483d493f9d47ce5458d1f183c29d
Adrian <134459802+adrianandbaton@users.noreply.github.com>
2025-06-10 03:48:26 +0000

    I have made my play_card subroutine more efficient by making other subroutines forf the action cards

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ deck = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 
 'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
 'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
 'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
-'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR']
+'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR', 'WD4', 'WD4']
 
 random.shuffle(deck)
 
@@ -55,19 +55,7 @@ def distribute_cards(number, deck):
     return player_deck
 
 def next_turn(player_deck, player1_deck, player2_deck):
-    valid = True
-    while valid:
-        turn = int(input("Who's turn is it? Player1 or player2?"))
-        if turn == 1: 
-            player_deck = player1_deck
-            valid = False
-        elif turn == 2:
-            player_deck = player2_deck
-            valid = False
-        else: 
-            print('Please enter a valid player!')
-            turn = int(input("Who's turn is it? Player1 or player2?"))
-
+    whos_turn(player_deck, player1_deck, player2_deck, deck)
     ready = input("Has the laptop passed to the next user? (Y?N)")
     while ready.upper() != 'Y':
         ready = input("OK, now has the laptop been passed (Y/N)")
@@ -90,13 +78,13 @@ def whos_turn(player_deck, player1_deck, player2_deck, deck):
             player_deck = player2_deck
             valid = False
         else: 
-            print('Please enter a valid player!')
-            turn = int(input("Who's turn is it? Player 1 or player 2?"))
+           print('Please enter a valid player!')
+           turn = int(input("Who's turn is it? Player 1 or player 2?"))
     return player_deck
 
 
 def play_card(player_deck, center_card, player1_deck, player2_deck, deck):
-    whos_turn(player_deck)
+    whos_turn(player_deck, player1_deck, player2_deck, deck)
     card = input("What card do you wish to place?, press 'd' to draw card")
     if card == 'd':
         draw_card(player_deck, deck)
@@ -119,12 +107,12 @@ def play_card(player_deck, center_card, player1_deck, player2_deck, deck):
 
     if card[1] == 'R':
         center_card = card
-        next_turn(player_deck, player1_deck, player2_deck)
-        next_turn(player_deck, player1_deck, player2_deck)
+        reverse(card ,player_deck, center_card, player1_deck, player2_deck, deck)
     if 'S' in card:
         center_card = card
-        next_turn(player_deck, player1_deck, player2_deck)
-        next_turn(player_deck, player1_deck, player2_deck)
+        skip(card ,player_deck, center_card, player1_deck, player2_deck, deck)
+    
+    return card, center_card
 
 
 def draw_two(player_deck, player1_deck, player2_deck, deck):
@@ -166,12 +154,22 @@ def wild_draw_four(player_deck, player1_deck, player2_deck, deck, center_card):
             print('Invalid colour, please reenter')
             center_colour = input('what colour do you want to change the center card to?')
 
+def reverse(card ,player_deck, center_card, player1_deck, player2_deck, deck): 
+        next_turn(player_deck, player1_deck, player2_deck)
+        next_turn(player_deck, player1_deck, player2_deck)
+
+def skip(card ,player_deck, center_card, player1_deck, player2_deck, deck):
+    next_turn(player_deck, player1_deck, player2_deck)
+    next_turn(player_deck, player1_deck, player2_deck)
+    
+    
+    
+
 
 def main(deck):
-    y = 0
-    center_card = deck[y]
+    center_card = deck[deck[random.randint(0,104)]]
     while '+2' in center_card or center_card[1] == 'R' or 'S' in center_card:
-        center_card = deck[y+1]
+        center_card = deck[deck[random.randint(0,104)]]
     player1_deck = distribute_cards(7,deck)
     player2_deck = distribute_cards(7,deck)
     print(player1_deck)
