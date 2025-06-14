e82590c8e9aad2bad6d64757391b0f5529e64603
Adrian <134459802+adrianandbaton@users.noreply.github.com>
2025-06-14 14:19:31 +0000

    finished the game starting to bug fix

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import random
-import os 
-deck = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
-'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
-'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
-'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR',
-'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 'RR', 
-'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS', 'YR',
-'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS', 'GR',
-'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'BR', 'WD4', 'WD4']
+import os
 
+deck = ['R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 
+'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS',
+'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS',
+'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS',
+'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6','R7', 'R8', 'R9', 'R+2', 'RS', 
+'Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6','Y7', 'Y8', 'Y9', 'Y+2', 'YS',
+'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6','G7', 'G8', 'G9', 'G+2', 'GS',
+'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6','B7', 'B8', 'B9', 'B+2', 'BS', 'WD4', 'WD4']
 random.shuffle(deck)
 
-rules = input('Do you want to review the rules?(Y/N)')
-while rules != "N" and rules != "Y":
-    rules = input("Please reenter with (Y/N)")
-if rules == 'Y':
-    print("""Each player has 7 cards 
+
+def introduction():
+    os.system('clear')
+    print("Welcome to UNO!")
+    print()
+    rules = input('Do you want to review the rules?(Y/N)')
+    while rules != "N" and rules != "Y":
+        rules = input("Please reenter with (Y/N)")
+    if rules == 'Y':
+        print("""Each player has 7 cards 
 
 First one to use all their cards win 
 
@@ -48,165 +53,98 @@ Skip everyone – goes back to your turn
 You cannot play after you draw
 
 If you want to stop the game, press 'q' at the end of each turn""")
-
-
-def distribute_cards(number, deck):
-    player_deck = []
+    else:
+        print("OK, let's start")
+def draw_card(player_deck, deck, number):
     for i in range(number):
-        player_deck.append(deck[random.randint(0,104)])
-    return player_deck
+        x = deck.pop(0)
+        player_deck.append(x)
+    return x,deck
 
-def next_turn(player_deck, player1_deck, player2_deck):
-    player_deck = whos_turn(player_deck, player1_deck, player2_deck, deck)
-    ready = input("Has the laptop passed to the next user? (Y?N)")
+    return player_deck
+def next_turn():
+    print("Please pass the laptop to another person")
+    ready = input("Has the laptop been passed? (Y/N)")
+
     while ready.upper() != 'Y':
-        ready = input("OK, now has the laptop been passed (Y/N)")
-    if ready  == 'Y':
-        os.system('clear')
-    elif ready == 'Q' or 'q':
-        quit()
-    return ready, player_deck, player1_deck, player2_deck
+        print("Ok Bro, has it been passed now?")
+    
+    os.system('clear')
+def play_card(main_deck, other_deck, deck, center_card, main_player, other_player):
+    print("Center card is {}".format(center_card))
+    repeat = False
+    print("It is now {}'s turn!".format(main_player))
+    print("This is your deck: ")
+    print(main_deck)
+    
+    card = input("Which card would you like to place, press d to draw: ")
 
-def draw_card(player_deck, deck):
-    x = deck[random.randint(0,104)]
-    player_deck.append(x)
-    return player_deck
+    while card not in main_deck and card != 'd':
+        print("That card is not in your deck")
+        card = input("Which card would you like to place, press d to draw: ")
 
-def whos_turn(player_deck, player1_deck, player2_deck, deck):
-    valid = True
-    while valid:
-        turn = int(input("Who's turn is it? Player 1 or player 2?"))
-        if turn == 1: 
-            player_deck = player1_deck
-            valid = False
-        elif turn == 2:
-            player_deck = player2_deck
-            valid = False
-        else: 
-           print('Please enter a valid player!')
-           turn = int(input("Who's turn is it? Player 1 or player 2?"))
-    return player_deck
+    if card in main_deck:
+        while card[0] != center_card[0] and card[1] != center_card[1]:
+            print("{} cannot be placed. Please enter again: ".format(card))
+            card = input("Which card would you like to place, press d to draw: ")
 
-
-def play_card(player_deck, center_card, player1_deck, player2_deck, deck):
-    player_deck = whos_turn(player_deck, player1_deck, player2_deck, deck)
-    card = input("What card do you wish to place?, press 'd' to draw card")
+            if card == 'd':
+                break
+        print("{} is successfully placed".format(card))
+        center_card = card
+    
     if card == 'd':
-        draw_card(player_deck, deck)
-
-    while card not in player_deck and card != 'd':
-        print("This card is not in your deck, please reenter")
-        card = input("What card do you wish to place?")
-
-    if card in player_deck:
-        while card[0] != center_card[0] and card[1] != center_card[1] and card != 'd':
-            card = input("This card is not valid, please reenter")
-        player_deck.remove(card)
-        center_card = card
-
-    if '+2' in card: 
-        center_card = card
-        next_turn(player_deck, player1_deck, player2_deck)
-        draw_two(player_deck)
-        next_turn(player_deck, player1_deck, player2_deck)
-
-    if card[1] == 'R':
-        center_card = card
-        reverse(card ,player_deck, center_card, player1_deck, player2_deck, deck)
-    if 'S' in card:
-        center_card = card
-        skip(card ,player_deck, center_card, player1_deck, player2_deck, deck)
+        card_drawn, deck = draw_card(main_deck, deck, 1)
+        print("You have drawn the card {}".format(card_drawn))
     
-    return card, center_card, player_deck, player1_deck, player2_deck
-
-
-def draw_two(player_deck, player1_deck, player2_deck, deck):
-    for i in range(2):
-        draw_one(player_deck, player1_deck, player2_deck, deck)
-
-def draw_one(player_deck, player1_deck, player2_deck, deck):
-    who = int(input('Which player is going to draw 2?'))
-    if who == 1: 
-        player_deck = player1_deck
-    else: 
-        player_deck = player2_deck
-    player_deck += deck[random.randint(0,104)]
-    return player_deck
-
-def draw_five(player_deck, player1_deck, player2_deck, deck, center_card):
-    for i in range(5):
-        draw_one(player_deck, player1_deck, player2_deck, deck)
-
-def wild_draw_four(player_deck, player1_deck, player2_deck, deck, center_card):
-    for i in range(4):
-        draw_one(player_deck, player1_deck, player2_deck, deck)
-    center_colour = input('what colour do you want to change the center card to?')
-    valid = True
-    while valid: 
-        if center_colour == 'Y':
-            center_card[0] = 'Y'
-            valid = False
-        elif center_colour == 'R':
-            center_card[0] = 'R'
-            valid = False
-        elif center_colour == 'G':
-            center_card[0] = 'G'
-            valid = False
-        elif center_colour == 'B':
-            center_card[0] = 'B'
-            valid = False
-        else: 
-            print('Invalid colour, please reenter')
-            center_colour = input('what colour do you want to change the center card to?')
-
-def reverse(card ,player_deck, center_card, player1_deck, player2_deck, deck): 
-        next_turn(player_deck, player1_deck, player2_deck)
-        next_turn(player_deck, player1_deck, player2_deck)
-
-def skip(card ,player_deck, center_card, player1_deck, player2_deck, deck):
-    next_turn(player_deck, player1_deck, player2_deck)
-    next_turn(player_deck, player1_deck, player2_deck)
+    if "+2" in card:
+        print("Since you placed a +2 card, 2 cards are added to {}'s deck".format(other_player))
+        card_drawn, deck = draw_card(other_deck, deck, 2)
+    
+    if "S" in card:
+        print("Since you have drawn the skip card, {}'s turn is skipped.".format(other_player))
+        repeat = True
+        
+    if repeat == True:
+        play_card(main_deck, other_deck, deck, center_card, main_player, other_player)
     
 
-def check_win(player_deck, player1_deck, player2_deck, deck):
-    player_deck = whos_turn(player_deck, player1_deck, player2_deck, deck)
-    if player1_deck == 0: 
-        print('Player 1 has won!')
-        return True
-    elif player2_deck == 0: 
-        print('Player 2 has won!') 
-        return True
-    else: 
-        return False
+def main(deck):
 
+    introduction()
+    print()
+    player1_deck = []
+    player2_deck = []
+    card_drawn, deck = draw_card(player1_deck, deck, 7)
+    crd_drawn, deck = draw_card(player2_deck, deck, 7)
 
+    player1 = input("Player 1, enter your name: ")
+    player2 = input("Player 2, enter your name: ")
+    center_card = deck.pop(0)
 
+    first = input("Who wants to go first?")
+    while first != player1 and first != player2:
+        print("Bro really? Not funny")
+        first = input("Who wants to go first?")
+    
+    if first == player1:
+        while len(player1_deck) != 0 and len(player2_deck) != 0:
+            play_card(player1_deck, player2_deck, deck, center_card, player1, player2)
+            next_turn()
+            play_card(player2_deck, player1_deck, deck, center_card, player2, player1)
+            next_turn
+    
+    elif first == player2:
+        while len(player1_deck) != 0 and len(player2_deck) != 0:
+            play_card(player2_deck, player1_deck, deck, center_card, player2, player1)
+            next_turn()
+            play_card(player1_deck, player2_deck, deck, center_card, player1, player2)
+            next_turn
+    
+    if len(player1_deck) == 0:
+        print("Congrats, {} won!!!".format(player1))
+    else:
+        print("What an unexpected turn of events, who would have believes that {} won???".format(player2))
+        print("GGs")
 
-
-def main(player_deck, player1_deck, player2_deck, deck):
-    center_card = deck[random.randint(0,104)]
-    while '+2' in center_card or center_card[1] == 'R' or 'S' in center_card:
-        center_card = deck[random.randint(0,104)]
-    player1_deck = distribute_cards(7,deck)
-    player2_deck = distribute_cards(7,deck)
-    print(player1_deck)
-    print(player2_deck)
-    while check_win(player_deck, player1_deck, player2_deck, deck)  == True: 
-        player_deck = whos_turn(player_deck, player1_deck, player2_deck, deck)
-        center_card, card, player1_deck, player2_deck, player_deck = play_card(player_deck, center_card, player1_deck, player2_deck, deck)
-        ready, player2_deck, player1_deck, player_deck = next_turn(player_deck, player1_deck, player2_deck)
-        player_deck = whos_turn(player_deck, player1_deck, player2_deck, deck)
-        center_card, card, player1_deck, player2_deck, player_deck = play_card(player_deck, center_card, player1_deck, player2_deck, deck)
-        ready, player2_deck, player1_deck, player_deck = next_turn(player_deck, player1_deck, player2_deck)
-
-
-
-
-
-
-
-
-player_deck = []
-player2_deck = []
-player1_deck = []
-main(player_deck, player1_deck, player2_deck, deck)
+main(deck)
